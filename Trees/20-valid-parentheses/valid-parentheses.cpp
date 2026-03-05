690ff35a3b44972e2a078dfdb28499553e2ae911
nikhil6611 <nikhiltiwari6611@gmail.com>
2026-03-05 23:24:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (87.74%) - LeetSync

---
+++ Trees/20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int i=0;
+        while(i<s.size()){
+            if(s[i]=='{' || s[i]=='['|| s[i]=='('){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if((s[i]=='}' && st.top()=='{') ||
+                    (s[i]==')' && st.top()=='(')||
+                    (s[i]==']' && st.top()=='[')){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+            }
+            i++;
+        }
+        return st.empty();
+    }
+};
